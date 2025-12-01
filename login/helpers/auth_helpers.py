df8159ec1905227c630f78bf20217d918ad02c41
Arslan Aslam <arslanaslam7282@gmail.com>
2025-12-01 19:50:36 +0500

    update

--- login/helpers/auth_helpers.py
+++ login/helpers/auth_helpers.py
@@ -214,4 +214,3 @@ class AuthHelpers:
     def navigate_to_signup(driver: WebDriver) -> bool:
         """Navigate to Sign Up page"""
         return ElementHelpers.safe_click(driver, [AuthHelpers.SIGN_UP_LINK])
-# asdf
